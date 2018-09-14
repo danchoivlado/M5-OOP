5d980449301ecd73017d2d14e5585509b04d7622
dani dani <dani_yordani@abv.bg>
2018-09-14 08:41:05 +0300

    moje 2 w

---
+++ functiona progrraming/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functiona_progrraming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split(new string[] {", "},StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToArray();
+
+            nums = nums.Select(x => x * 20).ToArray();
+            Console.WriteLine(string.Join(" ",nums));
+        }
+    }
+}
