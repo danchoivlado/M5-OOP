354d8f9ae902ecd158d7bab8fc2332b808bb14d7
dani dani <dani_yordani@abv.bg>
2018-09-14 19:18:49 +0300

    ConsoleColor

---
+++ InputOutput ColourChange/Console.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    public static class Console
+    {
+        public static void WriteLine(object text)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.WriteLine(text.ToString());
+            System.Console.ResetColor();
+        }
+
+        public static void Write(object text)
+        {
+            System.Console.ForegroundColor = ConsoleColor.Green;
+            System.Console.Write(text.ToString());
+            System.Console.ResetColor();
+        }
+
+        public static string ReadLine()
+        {
+           System.Console.ForegroundColor = ConsoleColor.Red;
+            return System.Console.ReadLine();
+        }
+    }
