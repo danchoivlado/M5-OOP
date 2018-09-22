d7ea873d6bcf66ac3264dc5d3c948f13d1414b4c
dani dani <dani_yordani@abv.bg>
2018-09-22 09:02:54 +0300

    ReCommit

---
+++ OOP BASIC Exams/ExamPawExam/test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split().Select(se=>int.Parse(se)).ToList();
+            var t = arr.RemoveAt(2);
+            Console.WriteLine(string.Join(" ",arr));
+        }
+    }
+}
