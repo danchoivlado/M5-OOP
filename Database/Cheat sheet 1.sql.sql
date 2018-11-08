06d27ceb74e21a6d2c59711a8cfac94cd02815f9
danchoivlado <35624043+danchoivlado@users.noreply.github.com>
2018-11-08 10:43:49 +0200

    Update Cheat sheet 1.sql.sql

--- Database/Cheat sheet 1.sql.sql
+++ Database/Cheat sheet 1.sql.sql
@@ -62,3 +62,18 @@ SELECT country_name,country_code,
 	ORDER BY country_name ASC;
 
 )
+
+CASE(
+SELECT e.employee_id,e.first_name,
+CASE 
+	WHEN p.start_date >= '2005-01-01' THEN NULL
+    ELSE p.name
+    END AS `project_name`
+FROM employees AS e
+INNER JOIN employees_projects AS ep
+ON e.employee_id = ep.employee_id
+INNER JOIN projects AS p
+ON ep.project_id = p.project_id
+WHERE e.employee_id = 24
+ORDER BY project_name ;
+)
